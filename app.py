54ace6775ac7667c2c9150333b87ed10df14ae00
griger <gustavo.rg92@gmail.com>
2016-11-28 20:52:14 +0100

    Añadiendo app de ejemplo. Closes #1

---
+++ app.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import numpy as np
+
+v = np.random.rand(100)
+print("El vector de numeros aleatorios:\n", v)
+v = v*2
+print("El vector de numeros aleatorios multiplicado por 2:\n", v)
